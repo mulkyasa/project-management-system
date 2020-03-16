3a28889b3a91f66fa6733606d82397aa6595f4cf
mulkyasa <mulkyasa@gmail.com>
2020-03-17 05:43:39 +0700

    login feature has been added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = (db) => {
+  router.get("/", function(req, res, next) {
+    res.render("login");
+  });
 
-module.exports = router;
+  router.post("/login", function(req, res, next) {
+    const { email, password } = req.body;
+    db.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password], (err, data) => {
+      if(err) return res.send(err);
+      if(data.rows.length > 0) {
+        if(data.rows[0].email == email && data.rows[0].password == password) {
+          res.redirect('/projects');
+        };
+      };
+    });
+  });
+
+  router.get('/projects', (req, res, nexst) => {
+    res.render('index', { user: 'saya' });
+  });
+
+  return router;
+};
