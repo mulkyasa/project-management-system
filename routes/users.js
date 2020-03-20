8fb859f325c299385e0689ae655646b62ec1bd29
mulkyasa <mulkyasa@gmail.com>
2020-03-20 17:06:46 +0700

    adding filter position and type job on users table

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,6 @@ module.exports = db => {
   /* GET users listing. */
   router.get("/", helpers.isLoggedIn, (req, res, next) => {
     let sql = `SELECT userid, email, CONCAT(firstname,' ',lastname) AS name, position, typejob FROM users`;
-    console.log(sql);
     // filter users
     let result = [];
     let filterData = false;
@@ -18,7 +17,11 @@ module.exports = db => {
       checkName,
       inputName,
       checkEmail,
-      inputEmail
+      inputEmail,
+      checkPosition,
+      inputPosition,
+      checkTypeJob,
+      inputTypeJob
     } = req.query;
 
     if (checkId && inputId) {
@@ -33,15 +36,23 @@ module.exports = db => {
       result.push(`email = '${inputEmail}'`);
       filterData = true;
     }
+    if (checkPosition && inputPosition) {
+      result.push(`position = '${inputPosition}'`);
+      filterData = true;
+    }
+    if (checkTypeJob && inputTypeJob) {
+      result.push(`typejob = ${inputTypeJob == 'Full Time' ? true : false}`);
+      filterData = true;
+    }
     if (filterData) {
       sql += ` WHERE ${result.join(" AND ")}`;
     }
+
 
     sql += ` ORDER BY userid`;
 
     db.query(sql, (err, data) => {
       const page = req.query.page || 1;
-      
 
       if (err) res.status(500).json(err);
 
@@ -65,9 +76,8 @@ module.exports = db => {
 
   router.post("/add", helpers.isLoggedIn, (req, res, next) => {
     const { email, password, firstname, lastname, position, typejob } = req.body;
-    const job = typejob == "Full Time" ? true : false;
-    const sql = `INSERT INTO users (email, password, firstname, lastname, position, typejob) VALUES ('${email}', '${password}', '${firstname}', '${lastname}', '${position}', ${job})`;
-    db.query(sql, (err, data) => {
+    const isTypeJob = typejob == "Full Time" ? true : false;
+    db.query(`INSERT INTO users (email, password, firstname, lastname, position, typejob) VALUES ($1, $2, $3, $4, $5, $6)`, [email, password, firstname, lastname, position, isTypeJob], (err, data) => {
         if (err) res.status(500).json(err);
         res.redirect("/users");
       }
@@ -87,13 +97,15 @@ module.exports = db => {
   });
 
   router.post("/edit/:userid", helpers.isLoggedIn, (req, res, next) => {
-    const { firstname, lastname, email, password } = req.body;
+    const { firstname, lastname, email, password, position, typejob } = req.body;
+    const isTypeJob = typejob == 'Full Time' ? true : false;
     const { userid } = req.params;
     if (!password) {
-      sql = `UPDATE users SET firstname = '${firstname}', lastname = '${lastname}', email = '${email}' WHERE userid = ${userid}`;
+      sql = `UPDATE users SET firstname = '${firstname}', lastname = '${lastname}', email = '${email}', position = '${position}', typejob = ${isTypeJob} WHERE userid = ${userid}`;
     } else {
-      sql = `UPDATE users SET firstname = '${firstname}', lastname = '${lastname}', email = '${email}', password = '${password}' WHERE userid=${userid}`;
+      sql = `UPDATE users SET firstname = '${firstname}', lastname = '${lastname}', email = '${email}', password = '${password}', position = '${position}', typejob = ${isTypeJob} WHERE userid=${userid}`;
     }
+    console.log(sql);
     db.query(sql, (err, data) => {
       if (err) res.status(500).json(err);
       res.redirect("/users");
